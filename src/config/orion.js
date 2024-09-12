a247dd7cc84194d585569a63f14cb0fa132ecd16
Sharad Sinha <123639018+Sharad1013@users.noreply.github.com>
2024-09-12 12:28:54 +0530

    Update orion.js

--- src/config/orion.js
+++ src/config/orion.js
@@ -1,1 +1,42 @@
 // Just Copy the Gemini API from Gemini AI studio into this file and add your own API key.
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = import.meta.env.VITE_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  return result.response.text();
+}
+
+export default run;
